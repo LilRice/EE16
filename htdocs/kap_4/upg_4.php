09b8b9766c2785ad48b62f3869fc1aec15cb5a1b
Bat-Erdene Ganbat <bat@C02SV3D4G8WL.local>
2018-09-05 13:29:08 +0200

    Börjat öva på strängar, formulär.

---
+++ htdocs/kap_4/upg_4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="upg_4b.php" method="post">
+    <label>Tempratur</label> <input type="text" name="temp"> <br>
+    <label>Celcius</label> <input type="radio" name="omvandla"> OMVANDLA FROM NON RETARDED TO RETARDED&deg;<br>
+    <label>Faomvandla</label> <input type="radio" name="omvandla">  OMVANDLA FROM RETARDED TO NON RETARDED<br>
+    
+    <button>Omvandla</button>
+    
+    </form>
+
+    
+</body>
+</html>
